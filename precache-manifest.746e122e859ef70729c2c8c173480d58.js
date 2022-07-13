13299d6fb135cd0106cab547306d123a1013a262
Brenedin Gomez <brenedin.gomez@protonmail.com>
2022-07-13 15:34:41 -0600

    Updates

---
+++ precache-manifest.746e122e859ef70729c2c8c173480d58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "514923f0356f4b4298dc",
+    "url": "/React-ST-test/static/css/main.a321d4a5.chunk.css"
+  },
+  {
+    "revision": "514923f0356f4b4298dc",
+    "url": "/React-ST-test/static/js/main.514923f0.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/React-ST-test/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "5b12252cc6e3f10c3a4a",
+    "url": "/React-ST-test/static/js/2.5b12252c.chunk.js"
+  },
+  {
+    "revision": "30885ccaad4e5bb9d9d0",
+    "url": "/React-ST-test/static/js/runtime~main.30885cca.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/React-ST-test/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2e43872c05d61655bf2be0bb497c2f61",
+    "url": "/React-ST-test/index.html"
+  }
+];
